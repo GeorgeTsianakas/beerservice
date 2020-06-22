f1721e16996a8c0cfbf215ab6fadcd05fa4c15b1
George <gtsianakas94@gmail.com>
2020-06-22 15:26:53 +0300

    Update .gitignore
    Update .gitignore file

---
+++ target/generated-sources/annotations/com/beerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package com.beerservice.web.mappers;
+
+import javax.annotation.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-06-09T20:42:45+0300",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_201 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
